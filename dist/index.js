6847d58a601ab38d52160aeadb8628e5db1797a5
Damien Coraboeuf <damien.coraboeuf@collibra.com>
2022-05-04 14:51:42 +0200

    Mutation (missing mutation option)

--- dist/index.js
+++ dist/index.js
@@ -67,7 +67,7 @@ const setValidationDataByRunId = (clientEnvironment, config, logging) => {
     `;
     // Running the query
     client.mutate({
-        query: query,
+        mutation: query,
         variables: {
             owner: config.owner,
             repository: config.repository,
