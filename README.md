b7451c01d6ff5a75d9af7e7e0b0aca7d957602a0
Damien Coraboeuf <damien.coraboeuf@collibra.com>
2022-05-04 10:22:05 +0200

    Checks on basic arguments

--- README.md
+++ README.md
@@ -5,9 +5,21 @@ GitHub Action used to inject data into validations in Ontrack.
 
 ## Inputs
 
+### `owner`
+
+The name of the repository owner where the validation is to be set. If not set, the current repository owner will be used.
+
 ### `repository`
 
 The name of the repository where the validation is to be set. If not set, the current repository will be used.
+
+### `build-name`
+
+Name of the Ontrack build where to create the validation. If not set and if `build-label` is not set either, the current workflow run ID will be used to identify the build.
+
+### `build-label`
+
+Release property (label) of the Ontrack build where to create the validation. If not set and if `build-name` is not set either, the current workflow run ID will be used to identify the build.
 
 ## Example usage
 
--- index.js
+++ index.js
@@ -1,9 +1,33 @@
+// noinspection ExceptionCaughtLocallyJS
+
 const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
     // Getting all the arguments
-    const repository = core.getInput('repository');
+    let owner = core.getInput('owner');
+    let repository = core.getInput('repository');
+    const buildName = core.getInput('build-name');
+    const buildLabel = core.getInput('build-label');
+
+    // Setting the owner
+    if (!owner) {
+        owner = github.context.repo.owner;
+    }
+
+    // Setting the repository
+    if (!repository) {
+        repository = github.context.repo.repo;
+    }
+
+    // Build name & label cannot be specified together
+    if (buildName && buildLabel) {
+        throw Error('"build-name" and "build-label" cannot be both declared.');
+    }
+
+    // Logging
+    core.info(`Owner:      ${owner}`);
+    core.info(`Repository: ${repository}`);
 } catch (error) {
     core.setFailed(error.message);
 }
