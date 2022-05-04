3f9041d989f1adfd1a03d3834b391cf0fbd8c3af
Damien Coraboeuf <damien.coraboeuf@collibra.com>
2022-05-04 13:57:53 +0200

    Full async code

--- dist/index.js
+++ dist/index.js
@@ -21528,80 +21528,84 @@ const parseValidationData = async (type, path, logging) => {
     }
 };
 
-try {
-    // Getting all the arguments
-    const logging = core.getBooleanInput('logging');
-    let owner = core.getInput('owner');
-    let repository = core.getInput('repository');
-    const buildName = core.getInput('build-name');
-    const buildLabel = core.getInput('build-label');
-    let validation = core.getInput('validation');
-    const inputValidationData = core.getInput('validation-data');
-    const inputTestSummaryValidationData = core.getInput('test-summary-validation-data');
-    const inputMetricsValidationData = core.getInput('metrics-validation-data');
-    const fileValidationDataType = core.getInput('file-validation-data-type');
-    const fileValidationDataPath = core.getInput('file-validation-data-path');
+async function run() {
+    try {
+        // Getting all the arguments
+        const logging = core.getBooleanInput('logging');
+        let owner = core.getInput('owner');
+        let repository = core.getInput('repository');
+        const buildName = core.getInput('build-name');
+        const buildLabel = core.getInput('build-label');
+        let validation = core.getInput('validation');
+        const inputValidationData = core.getInput('validation-data');
+        const inputTestSummaryValidationData = core.getInput('test-summary-validation-data');
+        const inputMetricsValidationData = core.getInput('metrics-validation-data');
+        const fileValidationDataType = core.getInput('file-validation-data-type');
+        const fileValidationDataPath = core.getInput('file-validation-data-path');
 
-    // Setting the owner
-    if (!owner) {
-        owner = github.context.repo.owner;
-    }
+        // Setting the owner
+        if (!owner) {
+            owner = github.context.repo.owner;
+        }
 
-    // Setting the repository
-    if (!repository) {
-        repository = github.context.repo.repo;
-    }
+        // Setting the repository
+        if (!repository) {
+            repository = github.context.repo.repo;
+        }
 
-    // Build name & label cannot be specified together
-    if (buildName && buildLabel) {
-        throw Error('"build-name" and "build-label" cannot be both declared.');
-    }
+        // Build name & label cannot be specified together
+        if (buildName && buildLabel) {
+            throw Error('"build-name" and "build-label" cannot be both declared.');
+        }
 
-    // Validation name
-    if (!validation) {
-        validation = github.context.action;
-    }
+        // Validation name
+        if (!validation) {
+            validation = github.context.action;
+        }
 
-    // Extracting the validation data
-    let validationData = {};
-    if (inputValidationData) {
-        validationData = YAML.parse(inputValidationData);
-    } else if (inputTestSummaryValidationData) {
-        validationData = {
-            type: "net.nemerosa.ontrack.extension.general.validation.TestSummaryValidationDataType",
-            data: YAML.parse(inputTestSummaryValidationData)
-        };
-    } else if (inputMetricsValidationData) {
-        validationData = {
-            type: "net.nemerosa.ontrack.extension.general.validation.MetricsValidationDataType",
-            data: {
-                metrics: YAML.parse(inputMetricsValidationData)
-            }
-        };
-    } else if (fileValidationDataType && fileValidationDataPath) {
-        validationData = parseValidationData(fileValidationDataType, fileValidationDataPath, logging);
-    } else {
-        throw Error('No validation data has been passed.')
-    }
+        // Extracting the validation data
+        let validationData = {};
+        if (inputValidationData) {
+            validationData = YAML.parse(inputValidationData);
+        } else if (inputTestSummaryValidationData) {
+            validationData = {
+                type: "net.nemerosa.ontrack.extension.general.validation.TestSummaryValidationDataType",
+                data: YAML.parse(inputTestSummaryValidationData)
+            };
+        } else if (inputMetricsValidationData) {
+            validationData = {
+                type: "net.nemerosa.ontrack.extension.general.validation.MetricsValidationDataType",
+                data: {
+                    metrics: YAML.parse(inputMetricsValidationData)
+                }
+            };
+        } else if (fileValidationDataType && fileValidationDataPath) {
+            validationData = await parseValidationData(fileValidationDataType, fileValidationDataPath, logging);
+        } else {
+            throw Error('No validation data has been passed.')
+        }
 
-    // Logging
-    core.info(`Owner:       ${owner}`);
-    core.info(`Repository:  ${repository}`);
-    // Logging of the build identification
-    if (buildLabel) {
-        core.info(`Build label: ${buildLabel}`);
-    } else if (buildName) {
-        core.info(`Build name:  ${buildName}`);
-    } else {
-        core.info(`Run ID:      ${github.context.runId}`);
+        // Logging
+        core.info(`Owner:       ${owner}`);
+        core.info(`Repository:  ${repository}`);
+        // Logging of the build identification
+        if (buildLabel) {
+            core.info(`Build label: ${buildLabel}`);
+        } else if (buildName) {
+            core.info(`Build name:  ${buildName}`);
+        } else {
+            core.info(`Run ID:      ${github.context.runId}`);
+        }
+        // Logging the validation information
+        core.info(`Validation:  ${validation}`);
+        // Logging of the validation data
+        core.info(`Validation:\n${JSON.stringify(validationData)}`);
+    } catch (error) {
+        core.setFailed(error.message);
     }
-    // Logging the validation information
-    core.info(`Validation:  ${validation}`);
-    // Logging of the validation data
-    core.info(`Validation:\n${JSON.stringify(validationData)}`);
-} catch (error) {
-    core.setFailed(error.message);
 }
+
+run();
 
 })();
 
