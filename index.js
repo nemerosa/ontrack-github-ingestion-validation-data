f119f91dd03d703027d24913e831455d9edabafb
Damien Coraboeuf <damien.coraboeuf@collibra.com>
2022-05-04 11:21:14 +0200

    Displaying the actual data

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ try {
     // Logging the validation information
     core.info(`Validation:  ${validation}`);
     // Logging of the validation data
-    core.info(`Validation:\n${validationData}`);
+    core.info(`Validation:\n${YAML.stringify(validationData)}`);
 } catch (error) {
     core.setFailed(error.message);
 }
